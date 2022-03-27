a53923e425f1a9b76910ec10ab28a6d06d9ce6c2
KhaleKuzzaman <s1910979158@ru.ac.bd>
2022-03-27 13:02:39 +0600

    Second commit

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello");
+    printf("Hello\n");
+    printf("C programming language is mother programming language over all all programming language we will see\n");
     return 0;
 }
